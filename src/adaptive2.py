ac63281ca6cda279749374cc1e1d32aa72033a9a
douxiaotian <douxiantianpku@gmail.com>
2019-05-14 00:30:20 -0400

    Update adaptive2.py

--- src/adaptive2.py
+++ src/adaptive2.py
@@ -334,7 +334,7 @@ def sample_people(dataset, people_per_batch, images_per_person):
     num_per_class = []
     sampled_class_indices = []
     # Sample images from these classes until we have enough
-    while (len(image_paths)<nrof_images) and (i < class_indices):
+    while (len(image_paths)<nrof_images) and (i < nrof_classes):
 
         class_index = class_indices[i]
         nrof_images_in_class = len(dataset[class_index])
